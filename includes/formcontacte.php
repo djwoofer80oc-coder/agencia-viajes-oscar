cf0fc347081f5c4f4c5f221c037bd82b5df42fbb
Oscar Cortes <djwoofer80oc@gmail.com>
2026-06-05 11:28:38 +0200

    first commit

---
+++ includes/formcontacte.php
@@ -0,0 +1,18 @@
+<h1>Contacta amb nosaltres</h1>
+
+<form>
+
+    <label>Nom</label>
+    <input type="text">
+
+    <label>Email</label>
+    <input type="email">
+
+    <label>Missatge</label>
+    <textarea></textarea>
+
+    <button>
+        Enviar
+    </button>
+
+</form>
